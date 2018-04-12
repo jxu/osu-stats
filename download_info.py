34157535120157ade22fe77f2350f69fa3bc2fbe
jxu <7989982+jxu@users.noreply.github.com>
2018-04-12 16:10:56 -0400

    Testing resume functionality

--- download_info.py
+++ download_info.py
@@ -1,6 +1,8 @@
 import requests
 import csv
 import datetime
+import os.path
+import pickle
 
 
 def scrape_pages(set_ids):
@@ -21,6 +23,7 @@ def scrape_pages(set_ids):
         soup = BeautifulSoup(r.text, "html.parser")
         json_beatmapset = soup.find("script", id="json-beatmapset")
         submitted_date = json.loads(json_beatmapset.string)["submitted_date"]
+        print(r.url, submitted_date)
         submitted_dates.append(submitted_date)
 
     assert(len(submitted_dates) == len(set_ids))
@@ -28,14 +31,16 @@ def scrape_pages(set_ids):
 
 
 def download_map_info(api_path="api.key", tsv_path="data.tsv",
-                      scrape=True, verbose=True, testing=False, cache=True):
+                      seen_path="seen.pkl", scrape=True, _testing=False,
+                      resume=True):
     """Main function to download and write data table from API (and scraping).
     Scraping mode adds what scrape_pages returns.
+    If resume flag, tries to restart based on seen file (pickle)
     WARNING: Scraping is probably slow! 
     """
 
     API_KEY = open(api_path).read()
-    tsvfile = open(tsv_path, 'w', encoding="utf-8")
+    tsvfile = open(tsv_path, 'a', encoding="utf-8")
     since_date_str = "2007-10-07"  # Date to start at
     API_MAX_RESULTS = 500
 
@@ -44,12 +49,18 @@ def download_map_info(api_path="api.key", tsv_path="data.tsv",
     header_keys = None
     mysql_timestamp_format = "%Y-%m-%d %H:%M:%S"
     scrape_headers = ["submitted_date"]
-    if cache:
-        import requests_cache
-        requests_cache.install_cache("cache")  # Caches in cache.sqlite
+
 
     seen_beatmap_ids = set()
     set_id_dict = dict()  # (set_id: submitted_date) pairs
+
+    # Load or init seen structures
+    if os.path.isfile(seen_path):
+        print("Loading seen files", seen_path)
+        with open(seen_path, 'rb') as f:
+            header_seen, header_keys, seen_beatmap_ids, set_id_dict = pickle.load(f)
+
+
     tsvwriter = csv.writer(tsvfile, delimiter='\t', lineterminator='\n')
 
     # Requests session
@@ -74,7 +85,7 @@ def download_map_info(api_path="api.key", tsv_path="data.tsv",
             full_header = header_keys.copy()
             if scrape: full_header.extend(scrape_headers)
 
-            if verbose: print(full_header)
+            print("Full header", full_header)
             tsvwriter.writerow(full_header)
             header_seen = True
 
@@ -87,8 +98,7 @@ def download_map_info(api_path="api.key", tsv_path="data.tsv",
                     set_ids_to_request.add(set_id)
 
             set_ids_to_request = list(set_ids_to_request)  # Fix order
-            submitted_dates = scrape_pages(list(set_ids_to_request))
-            if verbose: print(submitted_dates)
+            submitted_dates = scrape_pages(set_ids_to_request)
 
             # Add responses to set_id_dict
             for (set_id, submitted_date) in zip(set_ids_to_request, submitted_dates):
@@ -122,17 +132,22 @@ def download_map_info(api_path="api.key", tsv_path="data.tsv",
                 tsvwriter.writerow(row)
                 seen_beatmap_ids.add(beatmap_id)
 
-                if verbose:
-                    # Progress info, not actually used in written file
-                    print("{} {} {} - {} [{}]".format(
-                        info_dict["approved_date"],
-                        info_dict["beatmap_id"],
-                        info_dict["artist"],
-                        info_dict["title"],
-                        info_dict["version"]))
+                # Progress info, not actually used in written file
+                print("{} {} {} - {} [{}]".format(
+                    info_dict["approved_date"],
+                    info_dict["beatmap_id"],
+                    info_dict["artist"],
+                    info_dict["title"],
+                    info_dict["version"]))
 
             else:
-                if verbose: print("Skipped (seen already)")
+                print("Skipped (seen {} already)".format(beatmap_id))
+
+
+        # Write seen values
+        with open(seen_path, 'wb') as f:
+            pickle.dump((header_seen, header_keys, seen_beatmap_ids, set_id_dict), f)
+        print("Wrote id pickle files")
 
 
         # When the API returns 500 results, last mapset may have diffs cut off.
@@ -147,14 +162,13 @@ def download_map_info(api_path="api.key", tsv_path="data.tsv",
             end_date -= datetime.timedelta(seconds=1)
         since_date_str = end_date.strftime(mysql_timestamp_format)
 
-        if verbose:
-            print('-' * 50)
+        print('-' * 50)
 
-        if testing: break
+        if _testing: break
 
 
     tsvfile.close()
 
 if __name__ == "__main__":
     #print(scrape_pages([1,3]))
-    download_map_info(verbose=True, scrape=True, testing=False)
+    download_map_info(scrape=True, _testing=False)
