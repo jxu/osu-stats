76ccf84d1629a9bcc8d275f6fae19d9ff10882e3
jxu <7989982+jxu@users.noreply.github.com>
2018-09-20 14:44:05 -0400

    Modify R script to save rankings as csv

--- rankings.R
+++ rankings.R
@@ -1,3 +1,12 @@
 library(jsonlite)
-rankings <- do.call("rbind", fromJSON("rankings.json"))
-ddply(rankings, .(user_id), nrow)
+library(plyr)
+# Read from JSON
+rankings <- unique(do.call("rbind", fromJSON("rankings.json")))
+
+# Convert values to correct data types
+rankings.intvar <-!(names(rankings) %in% c("date", "rank", "pp"))
+rankings[ , rankings.intvar] <- sapply(rankings[ , rankings.intvar], as.integer)
+rankings$pp <- as.numeric(rankings$pp)
+# convert datetime POSIXct 
+
+write.csv(rankings, "rankings.csv")
