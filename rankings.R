1069f00ce4af3533692bc2acefbc7aa5041573fc
jxu <7989982+jxu@users.noreply.github.com>
2019-08-02 23:05:21 -0400

    Process all gamemode rankings

--- rankings.R
+++ rankings.R
@@ -1,20 +1,29 @@
 library(tidyverse)
 library(jsonlite)
-library(plyr)
 
 # Read from JSON and remove duplicate rows
-rankings <- unique(fromJSON("rankings.json"))
+ranking_filenames <- c(rankings0 = "rankings0.json", 
+                       rankings1 = "rankings1.json", 
+                       rankings2 = "rankings2.json", 
+                       rankings3 = "rankings3.json")
+
+rankings <- ranking_filenames %>% lapply(fromJSON)
+rankings <- rankings %>% lapply(unique)
 
 # Convert values to correct data types
-rankings <- colwise(parse_guess)(rankings)
+rankings <- rankings %>% lapply(plyr::colwise(parse_guess))
 
-write_csv(rankings, "rankings.csv")
+write_csv(rankings$rankings0, "rankings0.csv")
+write_csv(rankings$rankings1, "rankings1.csv")
+write_csv(rankings$rankings2, "rankings2.csv")
+write_csv(rankings$rankings3, "rankings3.csv")
 
 # Basic analysis
-length(unique(rankings$beatmap_id))  # ~13000 unique std maps
+rankings0 <- rankings$rankings0
+length(unique(rankings0$beatmap_id))  # ~13000 unique std maps
 
 # Players with less than 100 top plays
-rankings %>% 
+rankings0 %>% 
   group_by(user_id) %>%
-  dplyr::summarize(n = n()) %>%
+  summarize(n = n()) %>%
   filter(n < 100)
